fb2fec9dd45aec3f1a9cde0a1aa5b011b46c66ec
ujihisa <ujihisa@gmail.com>
2014-07-09 19:12:38 -0700

    onEnable

--- src/cloft2/core.clj
+++ src/cloft2/core.clj
@@ -1,5 +1,14 @@
 (ns cloft2.core
-  (:gen-class))
+  (:require [clojure.string :as s]
+            [clojure.tools.nrepl.server :as nrepl.server])
+  (:gen-class
+    :name io.github.ujihisa.cloft2.ClojurePlugin
+    :extends org.bukkit.plugin.java.JavaPlugin
+    :implements [org.bukkit.event.Listener]
+    :exposes-methods {onEnable -onEnable}))
+
+(defn -onEnable [self]
+  (prn 'onEnable self))
 
 (defn -main
   "I don't do a whole lot ... yet."
