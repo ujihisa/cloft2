6be4e6194bcd26e963dae2f990ed369d9d14921d
ujihisa <ujihisa@gmail.com>
2014-07-09 18:43:30 -0700

    url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cloft2 "0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/ujihisa/cloft2"
   :license {:name "GPLv3+"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot cloft2.core
