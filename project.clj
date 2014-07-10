59866b88aac839623941f4636fa31555b8583e03
ujihisa <ujihisa@gmail.com>
2014-07-09 18:48:21 -0700

    dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,15 @@
 (defproject cloft2 "0.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/ujihisa/cloft2"
-  :license {:name "GPLv3+"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :license {:name "GNU GPL v3+"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
+  :repositories {"org.bukkit"
+                 "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "0.9.2"]
+                 [org.clojure/tools.nrepl "0.2.3"]
+                 [org.bukkit/bukkit "1.7.10-R0.1-SNAPSHOT"]]
   :main ^:skip-aot cloft2.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :min-lein-version "2.2.0")
