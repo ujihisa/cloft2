06e137c81cfd251be5f3eb6951446d3aa9ccbc38
ujihisa <ujihisa@gmail.com>
2014-07-13 11:23:49 -0700

    human readability

--- client/src/cloft2/core.clj
+++ client/src/cloft2/core.clj
@@ -4,8 +4,12 @@
 
 (defn remote-eval* [nrepl-client expr]
   (try
-    (-> (repl/message nrepl-client {:op "eval" :code (str expr)})
-      first :value println)
+    (doseq [resp (repl/message nrepl-client {:op "eval" :code (str expr)})]
+      (condp #(get %2 %1) resp
+        :value (println (:value resp))
+        :ex (println (:ex resp))
+        :err (println (:err resp))
+        (prn (:status resp))))
     (catch Exception e (-> e .printStackTrace))
     #_(finally (.close nrepl-conn))))
 
@@ -16,7 +20,7 @@
   (with-open [nrepl-conn (repl/connect :host "mck.supermomonga.com" :port 7888)]
     (let [nrepl-client (repl/client nrepl-conn 2000)]
       (remote-eval nrepl-client
-        (let [ujm (org.bukkit.Bukkit/getOfflinePlayer "ujm")]
+        (let [ujm (org.bukkit.Bukkit/getOfflinePlaye "ujm")]
           ujm))))
   (System/exit 0))
 ; vim: set lispwords=remote-eval :
