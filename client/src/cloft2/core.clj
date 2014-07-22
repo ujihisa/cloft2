3c387474161d88f582375733024ac62e8c9d2cf1
ujihisa <ujihisa@gmail.com>
2014-07-21 18:47:25 -0700

    cleanup

--- client/src/cloft2/core.clj
+++ client/src/cloft2/core.clj
@@ -2,22 +2,24 @@
   (:require [clojure.string :as s]
             [clojure.tools.nrepl :as nrepl]))
 
-(defn remote-eval* [nrepl-client expr]
+#_(defn remote-eval* [nrepl-client expr]
   )
 
-(defmacro remote-eval [nrepl-client & exprs]
+#_(defmacro remote-eval [nrepl-client & exprs]
   `(remote-eval* ~nrepl-client '(do ~@exprs)))
 
 (defn -main [& args]
   (with-open [nrepl-conn (nrepl/connect :host "0.0.0.0" :port 7888)]
     (let [nrepl-client (nrepl/client nrepl-conn 2000)]
-      (doseq [file-path ["/home/ujihisa/git/cloft2/client/src/cloft2/lib.clj"
-                         "/home/ujihisa/git/cloft2/client/src/cloft2/fast_dash.clj"
-                         "/home/ujihisa/git/cloft2/client/src/cloft2/kickory.clj"
-                         "/home/ujihisa/git/cloft2/client/src/cloft2/coal.clj"
-                         "/home/ujihisa/git/cloft2/client/src/cloft2/sneaking_jump.clj"
-                         "/home/ujihisa/git/cloft2/client/src/cloft2/safe_grass.clj"
-                         "/home/ujihisa/git/cloft2/client/src/cloft2/app.clj"]
+      (doseq [:let [basedir (System/getenv "PWD")]
+              file-path* ["src/cloft2/lib.clj"
+                          "src/cloft2/fast_dash.clj"
+                          "src/cloft2/kickory.clj"
+                          "src/cloft2/coal.clj"
+                          "src/cloft2/sneaking_jump.clj"
+                          "src/cloft2/safe_grass.clj"
+                          "src/cloft2/app.clj"]
+              :let [file-path (str basedir "/" file-path*)]
               resp (nrepl/message
                      nrepl-client
                      {:op "load-file" :file (slurp file-path) :file-path file-path})]
