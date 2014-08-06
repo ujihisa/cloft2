5841c438dbb0c7f09bd9de1659c6089c6b829820
ujihisa <ujihisa@gmail.com>
2014-08-05 23:27:02 -0700

    start craftbukkit

--- server/project.clj
+++ server/project.clj
@@ -6,11 +6,13 @@
   :repositories {"org.bukkit"
                  "http://repo.bukkit.org/service/local/repositories/snapshots/content/"}
   :dependencies [[org.bukkit/bukkit "1.7.10-R0.1-SNAPSHOT"]
+                 [org.bukkit/craftbukkit "1.7.10-R0.1-SNAPSHOT"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/core.incubator "0.1.3"]
                  #_[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.match "0.2.0"]
                  [clj-http "0.9.2"]]
+  :main org.bukkit.craftbukkit.Main
   :target-path "target/%s"
   :java-source-paths ["javasrc"]
   :profiles {:uberjar {:aot :all}}
